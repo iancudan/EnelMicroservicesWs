5dd1c40e730f9fe928410373b0a55487f3eb9ff3
iancudan <iancu_dansilviu@yahoo.ro>
2022-12-12 14:54:52 -0800

    --dev corsPolicyDeazctivated

--- src/main/java/com/enelWs/EnelMicroservicesWs/Config/WebSecurityConfig.java
+++ src/main/java/com/enelWs/EnelMicroservicesWs/Config/WebSecurityConfig.java
@@ -6,24 +6,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
-@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true)
 public class WebSecurityConfig {
 
     @Autowired
     UserDetailsService userDetailsService;
 
-    @Bean
-    CorsFilter corsFilter() {
-        CorsFilter filter = new CorsFilter();
-        return filter;
-    }
+
     @Bean
     AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
@@ -31,5 +30,15 @@ public class WebSecurityConfig {
         provider.setPasswordEncoder(new BCryptPasswordEncoder());
         return provider;
     }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+
+//
 
 }
--- src/main/java/com/enelWs/EnelMicroservicesWs/Controller/AnreSummaryController.java
+++ src/main/java/com/enelWs/EnelMicroservicesWs/Controller/AnreSummaryController.java
@@ -1,15 +1,12 @@
 package com.enelWs.EnelMicroservicesWs.Controller;
 
 import com.enelWs.EnelMicroservicesWs.Entity.AnreSummary;
-import com.enelWs.EnelMicroservicesWs.Entity.AnreSummaryHistory;
 import com.enelWs.EnelMicroservicesWs.Service.AnreSummaryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 @CrossOrigin
 @RestController
@@ -29,6 +26,7 @@ public class AnreSummaryController {
     }
 
     @GetMapping("/getHint/{id}")
+    @ResponseBody
     public String getHint(@PathVariable Long id){
         AnreSummary anreSummary = anreSummaryService.findById(id);
         String hint = anreSummaryService.getHint(anreSummary);
